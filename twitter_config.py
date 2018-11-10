1d32b35d790a4ec726aac832794e8f3473e10a39
A Smith <asmith221@gmail.com>
2018-11-10 14:26:30 -0500

    updated

---
+++ twitter_config.py
@@ -0,0 +1,6 @@
+# Twitter API Keys
+consumer_key = "3REpngub1g5pFnxOrmFy7RA7u"
+consumer_secret = "B8Z6Qp4NsxDsEblyUwTqrQAd4CoCMAPkM9R8a81tGrOcy5fwwm"
+access_token = "338862045-4oYBBr7iESvJCw7u9KEhUxBFIdSmMGwi9sohBk5z"
+access_token_secret = "yQHd23XHo5iIrhFuVdJLq2aXgT4kHbV2BK7sNvz5X2fKc"
+api_key="25bc90a1196e6f153eece0bc0b0fceb"
